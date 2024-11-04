d2c646da6bd80590de5e37a751dc5c3b86525b97
AnhsThu <honganh03bg@gmail.com>
2024-11-04 14:51:27 +0700

    Cap nhat entitis

---
+++ src/main/java/com/example/websitebanquanao/WebsitebanquanaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.websitebanquanao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitebanquanaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsitebanquanaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websitebanquanao/WebsitebanquanaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websitebanquanao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitebanquanaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
